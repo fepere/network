ea44bb645c2abb5accf2747e0b16530d7e952259
Fernando Pereira <34613604+fepere@users.noreply.github.com>
2019-10-29 11:15:26 -0300

    Create ubuntu-netplan.sh

---
+++ scripts/ubuntu-netplan.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# "update Ubuntu netplan configuration file..."
+sudo touch /etc/netplan/99-custom-dns.yaml
+sudo wget https://raw.githubusercontent.com/fepere/network/master/config/99-custom-dns.yaml -O /etc/netplan/99-custom-dns.yaml
+sudo netplan apply
+sudo systemd-resolve --status
