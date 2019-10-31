334cb2f213fb28d7fdeaf5421f3c4c5e9226a77f
Fernando Pereira <34613604+fepere@users.noreply.github.com>
2019-10-31 01:04:06 -0300

    Update rhel-dhclient.sh

--- scripts/rhel-dhclient.sh
+++ scripts/rhel-dhclient.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # The aim of this script is to configure Alstom DNS search servers to a VMs running RHEL 7.x 
 # Set dhclient.conf configuration
-sudo touch /etc/dhcp/dhclient.conf
-sudo echo "# Alstom DNS search servers" > /etc/dhcp/dhclient.conf
-sudo echo "supersede domain-search 'reddog.microsoft.com', 'tnext.loc', 'alstom.hub';" >> /etc/dhcp/dhclient.conf
+cat << EOF > /etc/dhcp/dhclient.conf
+supersede domain-search "reddog.microsoft.com", "tnext.loc", "alstom.hub";
+EOF
